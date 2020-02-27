847346dd93ad8866c94dbd8810ba33348051c204
knielbo <kln@cas.au.dk>
2020-02-27 09:21:52 +0100

    add try-cathcall

--- src/redlib/preprocessing/lemmatizer.py
+++ src/redlib/preprocessing/lemmatizer.py
@@ -9,6 +9,11 @@ class Lemmatizer:
         self.nlp = stanfordnlp.Pipeline(processors='tokenize,mwt,pos,lemma',lang=lang)
     
     def preprocess(self, text):
-        doc = self.nlp(text)
-        lemma = [word.lemma for sent in doc.sentences for word in sent.words if word.lemma is not None]
+        try:
+            doc = self.nlp(text)
+            lemma = [word.lemma for sent in doc.sentences for word in sent.words if word.lemma is not None]
+        except:
+            lemma = "nan"
+
         return " ".join(lemma)
+        
