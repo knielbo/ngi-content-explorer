c7aca93efcc4834aa850b5575918f0892756fbc2
knielbo <kln@cas.au.dk>
2020-02-27 09:20:51 +0100

    rm sample

--- src/qd-test.py
+++ src/qd-test.py
@@ -8,6 +8,8 @@ import ast
 from redlib.datasets import DatasetLoader
 from redlib.preprocessing import CaseFolder
 from redlib.preprocessing import UtcTime
+from redlib.preprocessing import Lemmatizer
+from redlib.preprocessing import RegxFilter
 
 def main():
     # get input
@@ -19,11 +21,19 @@ def main():
     tags = ast.literal_eval(args["tag"])
     #load data
     print("[INFO] loading data...")
+    le = Lemmatizer(tag="body", lang="en")
     cf = CaseFolder(tag="body", lower=True)
     ut = UtcTime(tag="created_utc")
-    dl = DatasetLoader(preprocessors=[cf, ut])
-    data = dl.load(jsonPath, tags, verbose=10e4)
-    
-    print(data.head(5))
+    # remove urls
+    re0 = RegxFilter(tag="body", pattern="\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*")
+    # remove non alphanumeric
+    re1 = RegxFilter(tag="body", pattern="\W+")
+    re2 = RegxFilter(tag="body", pattern="\d")
+    re3 = RegxFilter(tag="body", pattern="\s\s+")
+    #dl = DatasetLoader(preprocessors=None)
+    dl = DatasetLoader(preprocessors=[le, re0, cf, ut, re1, re2, re3])
+    data = dl.load(jsonPath, tags, verbose=10e2)
+
+    data.to_csv(jsonPath, index=False)
 if __name__=="__main__":
     main()
